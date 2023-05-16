3eba205b37d3f32ff86dabf5094092ff8c2a05c0
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-05-16 12:41:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 
 st.title('Streamlit Example')
-
-df = pd.DataFrame(np.random.randn(500,2) / [50,50] + [37.76, -122.4], columns=['lat', 'lon'])
-st.map(df)
 x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+df = pd.DataFrame(np.random.randn(x,2) / [50,50] + [37.76, -122.4], columns=['lat', 'lon'])
+st.map(df)
+
+#st.write(x, 'squared is', x * x)
